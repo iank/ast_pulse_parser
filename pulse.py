53b0d1e26cbd2dba207f18da564f4623dce798bf
Ian Kilgore <iank@iank.org>
2020-02-04 19:06:01 -0700

    It works

--- pulse.py
+++ pulse.py
@@ -1,31 +1,74 @@
-from pprint import pprint
 import ast
 
-def main():
-    f = 'pulse(abc, 10*(4+10), time=4) + abcd(np.whatever(10, 11)) + pulse(4)'
-    tree = ast.parse(f)
+####
+# We accept calls to pulse, or sums of calls to pulse. Arguments can be
+# whatever, but the top level expression is fairly restricted.
+#
+# !! danger danger danger this thing evals its input don't use it !!
+####
 
-    pprint(ast.dump(tree.body[0].value))
+def parse(text):
+    expr = ast.parse(text, mode='eval');
+    
+    assert isinstance(expr, ast.Expression), "Top level isn't Expression"
 
-    analyzer = Analyzer()
-    analyzer.visit(tree)
-    analyzer.report()
+    calls = extractCalls(expr.body)
 
-def fun(x):
-    return x+2
+    pulses = map(callToPulse, calls)
 
-class Analyzer(ast.NodeVisitor):
-    def __init__(self):
-        self.stats = {"call": []}
+    return pulses
 
-    def visit_Call(self, node):
-        if isinstance(node.func, ast.Name):
-            self.stats["call"].append(node.func.id)
-        self.generic_visit(node)
+def callToPulse(call):
+    """Converts an ast.Call with two arguments into a Pulse
 
-    def report(self):
-        pprint(self.stats)
+    Note that the arguments will be evaluated. Input must be trusted.
+    Possible to use ast.literal_eval for untrusted input, but
+    input won't be able to e.g. call numpy
+    """
 
+    assert isinstance(call, ast.Call), "Expected ast.Call"
+    assert isinstance(call.func, ast.Name)  # np.whatever() vs whatever()
+    assert call.func.id == "pulse", "Only calls to pulse() supported"
 
-if __name__ == "__main__":
-    main()
+    assert len(call.args) == 2, "Expected 2 arguments"
+
+    time = evalArg(call.args[0])
+    width = evalArg(call.args[1])
+
+    return Pulse(time, width)
+
+def evalArg(arg):
+    """Evaluates an AST node as an Expression"""
+    return eval(compile(ast.Expression(arg), filename='', mode='eval'))
+
+def extractCalls(expr):
+    """Extract calls to pulse().
+
+    Expects either a bare ast.Call to pulse() or a sum of calls
+    in the form of nested BinOps.
+
+    Returns a list of ast.Call
+    """
+
+    assert isinstance(expr, ast.BinOp) or isinstance(expr, ast.Call), \
+         "Expected either a BinOp or Call"
+    
+    if isinstance(expr, ast.Call):
+        return [expr]
+    elif isinstance(expr, ast.BinOp):
+        #TODO assert operation is +
+        # The sum just flattens the list so we get [1,2,3,4] instead of
+        # [[1,2],[3,4]] for "(1+2)+(3+4)"
+        return sum(
+            [extractCalls(expr.left), extractCalls(expr.right)], []
+        )
+    else:
+        raise TypeError("Expected either a BinOp or Call")
+
+class Pulse(object):
+    def __init__(self, time, width):
+        self.time = time
+        self.width = width
+
+    def __eq__(self, other):
+        return self.width == other.width and self.time == other.time
--- test.py
+++ test.py
@@ -2,8 +2,34 @@ import pulse
 import unittest
 
 class ParseTest(unittest.TestCase):
-    def test_pulse(self):
-        self.assertEqual(pulse.fun(3), 5)
+    def test_pulse_equality(self):
+        self.assertEqual([pulse.Pulse(1, 2)], [pulse.Pulse(1, 2)])
+
+    def test_simple_pulse(self):
+        text = "pulse(3, 2)"
+        self.assertEqual(pulse.parse(text), [pulse.Pulse(3, 2)])
+
+    def test_sum_of_pulses(self):
+        text = "pulse(3, 2) + pulse(1, 4) + pulse(3, 8)"
+        self.assertEqual(pulse.parse(text), [
+            pulse.Pulse(3, 2),
+            pulse.Pulse(1, 4),
+            pulse.Pulse(3, 8),
+        ])
+
+    def test_sum_of_pulses_with_parens(self):
+        """This changes the form of the top level BinOp.."""
+        text = "(pulse(3, 2) + pulse(1, 4)) + pulse(3, 8)"
+        self.assertEqual(pulse.parse(text), [
+            pulse.Pulse(3, 2),
+            pulse.Pulse(1, 4),
+            pulse.Pulse(3, 8),
+        ])
+
+
+    def test_pulse_with_complex_args(self):
+        text = "pulse(3, sum([1, 2*3-5]))"
+        self.assertEqual(pulse.parse(text), [pulse.Pulse(3, 2)])
 
 if __name__ == '__main__':
     unittest.main()
