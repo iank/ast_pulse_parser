a710308f7079a4a1888e1f368b6e8afe59b9699e
Megan Kratz <meganbkratz@gmail.com>
2020-02-05 12:44:39 -0700

    added test cases and units file

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import pulse
 import unittest
-import numpy as np
 
 class ParseTest(unittest.TestCase):
     def test_pulse_equality(self):
@@ -9,10 +8,6 @@ class ParseTest(unittest.TestCase):
     def test_simple_pulse(self):
         text = "pulse(3, 2)"
         self.assertEqual(pulse.parse(text), [pulse.Pulse(3, 2)])
-
-    def test_pulse_with_np(self):
-        text = "pulse(3, np.linspace(0, 10, 10)[4])"
-        self.assertEqual(pulse.parse(text), [pulse.Pulse(3, np.linspace(0, 10, 10)[4])])
 
     def test_sum_of_pulses(self):
         text = "pulse(3, 2) + pulse(1, 4) + pulse(3, 8)"
@@ -31,10 +26,17 @@ class ParseTest(unittest.TestCase):
             pulse.Pulse(3, 8),
         ])
 
-
     def test_pulse_with_complex_args(self):
         text = "pulse(3, sum([1, 2*3-5]))"
         self.assertEqual(pulse.parse(text), [pulse.Pulse(3, 2)])
 
 if __name__ == '__main__':
     unittest.main()
+
+{'Amplitude': 'Amplitude'}
+
+
+'pulse(0.10, 0.500, Amplitude)' -> [(0.10, 0.500, 'Amplitude')]
+'pulse(0.01, 0.01, 10*mV) + \npulse(\n  times=0.1 + np.linspace(0, 0.01 * (10-1), 10),\n  widths=0.005,\n  values=20*mV\n) + \npulse(0.25, 0.014, Pulse2_amplitude)'
+        ->[(0.01, 0.01, 0.01), ([0.1 , 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19], 0.005, 0.02), (0.25, 0.014, 'Pulse2_amplitude')]
+'pulse(widths=0.01, times=[0.1,0.2,0.3], values=30*mV)' -> [([0.1,0.2,0.3], 0.01, 0.03)]
