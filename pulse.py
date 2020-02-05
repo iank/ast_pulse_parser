a674d97ebdb6d2bae68d0bbaa0e4f1941d62ce21
Ian Kilgore <iank@iank.org>
2020-02-05 13:41:13 -0700

    Made it classier

--- pulse.py
+++ pulse.py
@@ -1,5 +1,6 @@
 import ast
 import numpy
+import units
 
 ####
 # We accept calls to pulse, or sums of calls to pulse. Arguments can be
@@ -8,68 +9,108 @@ import numpy
 # !! danger danger danger this thing evals its input don't use it !!
 ####
 
-def parse(text):
-    expr = ast.parse(text, mode='eval');
+class PulseParser(object):
+    def __init__(self, vars={}):
+        # User-provided variables
+        self.vars = vars
+
+        # acq4 units
+        self.vars.update(units.allUnits)
+
+        # numpy
+        self.vars['np'] = numpy
+
+    def addVar(self, var, value=None):
+        """Add variable to eval namespace. If value is None'
+        variable will evaluate to its own name as a string"""
+
+        if value is None:
+            self.vars[var] = var
+        else:
+            self.vars[var] = value
+
+    def parse(self, text):
+        text = text.replace("\n", '').replace("\r", '')
+
+        expr = ast.parse(text, mode='eval');
     
-    assert isinstance(expr, ast.Expression), "Top level isn't Expression"
+        assert isinstance(expr, ast.Expression), \
+            "Top level isn't Expression"
 
-    calls = extractCalls(expr.body)
-    pulses = list(map(callToPulse, calls))
+        calls = self.extractCalls(expr.body)
+        pulses = list(map(self.callToPulse, calls))
 
-    return pulses
+        return pulses
 
-def callToPulse(call):
-    """Converts an ast.Call with two arguments into a Pulse
+    def callToPulse(self, call):
+        """Converts an ast.Call with two arguments into a Pulse
 
-    Note that the arguments will be evaluated. Input must be trusted.
-    Possible to use ast.literal_eval for untrusted input, but
-    input won't be able to e.g. call numpy
-    """
+        Note that the arguments will be evaluated. Input must be trusted.
+        Possible to use ast.literal_eval for untrusted input, but
+        input won't be able to e.g. call numpy
+        """
 
-    assert isinstance(call, ast.Call), "Expected ast.Call"
-    assert isinstance(call.func, ast.Name)  # np.whatever() vs whatever()
-    assert call.func.id == "pulse", "Only calls to pulse() supported"
+        assert isinstance(call, ast.Call), "Expected ast.Call"
+        assert isinstance(call.func, ast.Name)
+        assert call.func.id == "pulse", "Only calls to pulse() supported"
 
-    assert len(call.args) == 2, "Expected 2 arguments"
+        assert len(call.args + call.keywords) == 3, \
+            "Expected 3 arguments"
 
-    time = evalArg(call.args[0])
-    width = evalArg(call.args[1])
+        # Merge positional and keyword arguments into args dictionary
+        args = {}
+        keywords = {0: 'times', 1: 'widths', 2: 'values'}
+        for index, value in enumerate(call.args):   # Positional args
+            args[keywords[index]] = value
+        
+        for keyword in call.keywords:               # Keyword args
+            args[keyword.arg] = keyword.value
 
-    return Pulse(time, width)
+        # Eval args
+        for keyword, value in args.items():
+            args[keyword] = self.evalArg(value)
 
-def evalArg(arg):
-    """Evaluates an AST node as an Expression"""
-    return eval(compile(ast.Expression(arg), filename='', mode='eval'),
-                {'np': numpy})
-
-def extractCalls(expr):
-    """Extract calls to pulse().
-
-    Expects either a bare ast.Call to pulse() or a sum of calls
-    in the form of nested BinOps.
-
-    Returns a list of ast.Call
-    """
-
-    assert isinstance(expr, ast.BinOp) or isinstance(expr, ast.Call), \
-         "Expected either a BinOp or Call"
-    
-    if isinstance(expr, ast.Call):
-        return [expr]
-    elif isinstance(expr, ast.BinOp):
-        assert isinstance(expr.op, ast.Add), "Only addition in top level"
-        # The sum just flattens the list so we get [1,2,3,4] instead of
-        # [[1,2],[3,4]] for "(1+2)+(3+4)"
-        return sum(
-            [extractCalls(expr.left), extractCalls(expr.right)], []
+        # Construct pulse
+        return pulse(
+            times=args['times'],
+            widths=args['widths'],
+            values=args['values']
         )
-    else:
-        raise TypeError("Expected either a BinOp or Call")
 
-class Pulse(object):
-    def __init__(self, time, width):
-        self.time = time
-        self.width = width
+    def evalArg(self, arg):
+        """Evaluates an AST node as an Expression"""
+        return eval(compile(
+            ast.Expression(arg), filename='', mode='eval'
+        ), self.vars)
 
-    def __eq__(self, other):
-        return self.width == other.width and self.time == other.time
+    def extractCalls(self, expr):
+        """Extract calls to pulse().
+
+        Expects either a bare ast.Call to pulse() or a sum of calls
+        in the form of nested BinOps.
+
+        Returns a list of ast.Call
+        """
+
+        assert isinstance(expr, ast.BinOp) or \
+               isinstance(expr, ast.Call), \
+               "Expected either a BinOp or Call"
+    
+        if isinstance(expr, ast.Call):
+            return [expr]
+        elif isinstance(expr, ast.BinOp):
+            assert isinstance(expr.op, ast.Add), \
+                "Only addition in top level"
+            # The sum just flattens the list so we get [1,2,3,4]
+            # instead of [[1,2],[3,4]] for "(1+2)+(3+4)"
+            return sum([
+                self.extractCalls(expr.left),
+                self.extractCalls(expr.right)
+            ], [])
+        else:
+            raise TypeError("Expected either a BinOp or Call")
+
+
+def pulse(times, widths, values):
+    return (times, widths, values)
+
--- test.py
+++ test.py
@@ -1,42 +1,51 @@
 import pulse
 import unittest
+import numpy as np
 
+VARS = {
+    'Amplitude': 'Amplitude',
+    'Pulse2_amplitude': 'Pulse2_amplitude',
+}
 class ParseTest(unittest.TestCase):
-    def test_pulse_equality(self):
-        self.assertEqual([pulse.Pulse(1, 2)], [pulse.Pulse(1, 2)])
+    def setUp(self):
+        self.parser = pulse.PulseParser(VARS)
 
     def test_simple_pulse(self):
-        text = "pulse(3, 2)"
-        self.assertEqual(pulse.parse(text), [pulse.Pulse(3, 2)])
+        text = 'pulse(0.10, 0.500, Amplitude)'
+        expected = [(0.10, 0.500, 'Amplitude')]
+        got = self.parser.parse(text)
+        self.assertEqual(got, expected)
 
-    def test_sum_of_pulses(self):
-        text = "pulse(3, 2) + pulse(1, 4) + pulse(3, 8)"
-        self.assertEqual(pulse.parse(text), [
-            pulse.Pulse(3, 2),
-            pulse.Pulse(1, 4),
-            pulse.Pulse(3, 8),
-        ])
+    def test_pulse_some_keyword_args(self):
+        text = 'pulse(0.10, 0.500, values=Amplitude)'
+        expected = [(0.10, 0.500, 'Amplitude')]
+        got = self.parser.parse(text)
+        self.assertEqual(got, expected)
 
-    def test_sum_of_pulses_with_parens(self):
-        """This changes the form of the top level BinOp.."""
-        text = "(pulse(3, 2) + pulse(1, 4)) + pulse(3, 8)"
-        self.assertEqual(pulse.parse(text), [
-            pulse.Pulse(3, 2),
-            pulse.Pulse(1, 4),
-            pulse.Pulse(3, 8),
-        ])
+    def test_sum_of_pulses_with_units_and_keywords(self):
+        text = "pulse(0.01, 0.01, 10*mV) + \npulse(\n  times=0.1 + np.linspace(0, 0.01 * (10-1), 10),\n  widths=0.005,\n  values=20*mV\n) + \npulse(0.25, 0.014, Pulse2_amplitude)"
+        expected = [(0.01, 0.01, 0.01), ([0.1 , 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19], 0.005, 0.02), (0.25, 0.014, 'Pulse2_amplitude')]
+        got = self.parser.parse(text)
+        self.assertEqual(got[0], expected[0])
+        self.assertEqual(got[2], expected[2])
 
-    def test_pulse_with_complex_args(self):
-        text = "pulse(3, sum([1, 2*3-5]))"
-        self.assertEqual(pulse.parse(text), [pulse.Pulse(3, 2)])
+        self.assertEqual(got[1][2], expected[1][2])
+        self.assertEqual(got[1][1], expected[1][1])
+        self.assertTrue(np.allclose(got[1][0], expected[1][0]))
+
+    def test_pulse_with_keywords_order(self):
+        text = 'pulse(widths=0.01, times=[0.1,0.2,0.3], values=30*mV)'
+        expected = [([0.1,0.2,0.3], 0.01, 0.03)]
+        got = self.parser.parse(text)
+        self.assertEqual(got, expected)
+
+    def test_pulse_with_addVar(self):
+        text = 'pulse(widths=dogs, times=[0.1,0.2,0.3], values=30*cats)'
+        expected = [([0.1,0.2,0.3], 'dogs', 300)]
+        self.parser.addVar('cats', 10)
+        self.parser.addVar('dogs')
+        got = self.parser.parse(text)
+        self.assertEqual(got, expected)
 
 if __name__ == '__main__':
     unittest.main()
-
-{'Amplitude': 'Amplitude'}
-
-
-'pulse(0.10, 0.500, Amplitude)' -> [(0.10, 0.500, 'Amplitude')]
-'pulse(0.01, 0.01, 10*mV) + \npulse(\n  times=0.1 + np.linspace(0, 0.01 * (10-1), 10),\n  widths=0.005,\n  values=20*mV\n) + \npulse(0.25, 0.014, Pulse2_amplitude)'
-        ->[(0.01, 0.01, 0.01), ([0.1 , 0.11, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19], 0.005, 0.02), (0.25, 0.014, 'Pulse2_amplitude')]
-'pulse(widths=0.01, times=[0.1,0.2,0.3], values=30*mV)' -> [([0.1,0.2,0.3], 0.01, 0.03)]
